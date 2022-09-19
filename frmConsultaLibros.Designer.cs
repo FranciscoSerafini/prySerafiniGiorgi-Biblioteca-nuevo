ef2fb9fd4befd1d535f0d52c73f555b5720ce496
FranciscoSerafinGiorgi <franciscoserafinigiorgi@gmail.com>
2022-09-19 17:52:55 -0300

    Cambios nuevo formulario de libros

--- frmConsultaLibros.Designer.cs
+++ frmConsultaLibros.Designer.cs
@@ -28,45 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCodigoDistri = new System.Windows.Forms.TextBox();
+            this.txtCodigoAutor = new System.Windows.Forms.TextBox();
+            this.txtNombreLibro = new System.Windows.Forms.TextBox();
+            this.txtCodigoEdito = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.cmdListarLibros = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 33);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "AVANZAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "RETROCEDER";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtCodigoDistri
+            // 
+            this.txtCodigoDistri.Location = new System.Drawing.Point(160, 228);
+            this.txtCodigoDistri.Name = "txtCodigoDistri";
+            this.txtCodigoDistri.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoDistri.TabIndex = 23;
+            // 
+            // txtCodigoAutor
+            // 
+            this.txtCodigoAutor.Location = new System.Drawing.Point(160, 189);
+            this.txtCodigoAutor.Name = "txtCodigoAutor";
+            this.txtCodigoAutor.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoAutor.TabIndex = 22;
+            // 
+            // txtNombreLibro
+            // 
+            this.txtNombreLibro.Location = new System.Drawing.Point(160, 109);
+            this.txtNombreLibro.Name = "txtNombreLibro";
+            this.txtNombreLibro.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreLibro.TabIndex = 21;
+            // 
+            // txtCodigoEdito
+            // 
+            this.txtCodigoEdito.Location = new System.Drawing.Point(160, 149);
+            this.txtCodigoEdito.Name = "txtCodigoEdito";
+            this.txtCodigoEdito.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoEdito.TabIndex = 20;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(160, 69);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(10, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "CODIGO DISTRUIDOR";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "CODIGO AUTOR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "NOMBRE DEL LIBRO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "CODIGO EDITORIAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "CODIGO";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(74, 9);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Consulta de libros";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(39, 53);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(279, 588);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            // 
-            // cmdListarLibros
-            // 
-            this.cmdListarLibros.Location = new System.Drawing.Point(243, 656);
-            this.cmdListarLibros.Name = "cmdListarLibros";
-            this.cmdListarLibros.Size = new System.Drawing.Size(75, 23);
-            this.cmdListarLibros.TabIndex = 2;
-            this.cmdListarLibros.UseVisualStyleBackColor = true;
-            this.cmdListarLibros.Click += new System.EventHandler(this.cmdListarLibros_Click);
+            this.label1.Size = new System.Drawing.Size(294, 26);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "CONSULTE POR SU LIBRO";
             // 
             // frmConsultaLibros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 719);
-            this.Controls.Add(this.cmdListarLibros);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(309, 322);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCodigoDistri);
+            this.Controls.Add(this.txtCodigoAutor);
+            this.Controls.Add(this.txtNombreLibro);
+            this.Controls.Add(this.txtCodigoEdito);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frmConsultaLibros";
             this.Text = "frmConsultaLibros";
@@ -78,8 +184,18 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCodigoDistri;
+        private System.Windows.Forms.TextBox txtCodigoAutor;
+        private System.Windows.Forms.TextBox txtNombreLibro;
+        private System.Windows.Forms.TextBox txtCodigoEdito;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button cmdListarLibros;
     }
 }

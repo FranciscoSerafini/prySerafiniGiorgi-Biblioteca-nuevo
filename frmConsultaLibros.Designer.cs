62762811c1ed6093a4c9d557e8a989e3886c1ea9
FranciscoSerafinGiorgi <franciscoserafinigiorgi@gmail.com>
2022-09-20 18:52:52 -0300

    Se realizaron cambios

--- frmConsultaLibros.Designer.cs
+++ frmConsultaLibros.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.cmdAvanzar = new System.Windows.Forms.Button();
+            this.cmdRetroceso = new System.Windows.Forms.Button();
             this.txtCodigoDistri = new System.Windows.Forms.TextBox();
             this.txtCodigoAutor = new System.Windows.Forms.TextBox();
             this.txtNombreLibro = new System.Windows.Forms.TextBox();
@@ -43,66 +43,67 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button2
+            // cmdAvanzar
             // 
-            this.button2.Location = new System.Drawing.Point(179, 266);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(81, 33);
-            this.button2.TabIndex = 25;
-            this.button2.Text = "AVANZAR";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.cmdAvanzar.Location = new System.Drawing.Point(260, 316);
+            this.cmdAvanzar.Name = "cmdAvanzar";
+            this.cmdAvanzar.Size = new System.Drawing.Size(81, 33);
+            this.cmdAvanzar.TabIndex = 25;
+            this.cmdAvanzar.Text = "AVANZAR";
+            this.cmdAvanzar.UseVisualStyleBackColor = true;
+            this.cmdAvanzar.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // cmdRetroceso
             // 
-            this.button1.Location = new System.Drawing.Point(10, 266);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 33);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "RETROCEDER";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cmdRetroceso.Location = new System.Drawing.Point(12, 316);
+            this.cmdRetroceso.Name = "cmdRetroceso";
+            this.cmdRetroceso.Size = new System.Drawing.Size(92, 33);
+            this.cmdRetroceso.TabIndex = 24;
+            this.cmdRetroceso.Text = "RETROCEDER";
+            this.cmdRetroceso.UseVisualStyleBackColor = true;
+            this.cmdRetroceso.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtCodigoDistri
             // 
-            this.txtCodigoDistri.Location = new System.Drawing.Point(160, 228);
+            this.txtCodigoDistri.Location = new System.Drawing.Point(167, 268);
             this.txtCodigoDistri.Name = "txtCodigoDistri";
-            this.txtCodigoDistri.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoDistri.Size = new System.Drawing.Size(174, 20);
             this.txtCodigoDistri.TabIndex = 23;
             // 
             // txtCodigoAutor
             // 
-            this.txtCodigoAutor.Location = new System.Drawing.Point(160, 189);
+            this.txtCodigoAutor.Location = new System.Drawing.Point(167, 231);
             this.txtCodigoAutor.Name = "txtCodigoAutor";
-            this.txtCodigoAutor.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoAutor.Size = new System.Drawing.Size(174, 20);
             this.txtCodigoAutor.TabIndex = 22;
             // 
             // txtNombreLibro
             // 
-            this.txtNombreLibro.Location = new System.Drawing.Point(160, 109);
+            this.txtNombreLibro.Location = new System.Drawing.Point(167, 108);
+            this.txtNombreLibro.Multiline = true;
             this.txtNombreLibro.Name = "txtNombreLibro";
-            this.txtNombreLibro.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreLibro.Size = new System.Drawing.Size(174, 69);
             this.txtNombreLibro.TabIndex = 21;
             // 
             // txtCodigoEdito
             // 
-            this.txtCodigoEdito.Location = new System.Drawing.Point(160, 149);
+            this.txtCodigoEdito.Location = new System.Drawing.Point(167, 194);
             this.txtCodigoEdito.Name = "txtCodigoEdito";
-            this.txtCodigoEdito.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoEdito.Size = new System.Drawing.Size(174, 20);
             this.txtCodigoEdito.TabIndex = 20;
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(160, 69);
+            this.txtCodigo.Location = new System.Drawing.Point(167, 71);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.Size = new System.Drawing.Size(174, 20);
             this.txtCodigo.TabIndex = 19;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(10, 229);
+            this.label6.Location = new System.Drawing.Point(9, 271);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(152, 17);
             this.label6.TabIndex = 18;
@@ -112,7 +113,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(10, 189);
+            this.label5.Location = new System.Drawing.Point(12, 230);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(116, 17);
             this.label5.TabIndex = 17;
@@ -122,7 +123,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(10, 109);
+            this.label4.Location = new System.Drawing.Point(9, 110);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(144, 17);
             this.label4.TabIndex = 16;
@@ -132,7 +133,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(10, 149);
+            this.label3.Location = new System.Drawing.Point(9, 189);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(139, 17);
             this.label3.TabIndex = 15;
@@ -142,7 +143,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 69);
+            this.label2.Location = new System.Drawing.Point(9, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 17);
             this.label2.TabIndex = 14;
@@ -152,7 +153,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(35, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(294, 26);
             this.label1.TabIndex = 13;
@@ -162,9 +163,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 322);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(374, 374);
+            this.Controls.Add(this.cmdAvanzar);
+            this.Controls.Add(this.cmdRetroceso);
             this.Controls.Add(this.txtCodigoDistri);
             this.Controls.Add(this.txtCodigoAutor);
             this.Controls.Add(this.txtNombreLibro);
@@ -186,8 +187,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cmdAvanzar;
+        private System.Windows.Forms.Button cmdRetroceso;
         private System.Windows.Forms.TextBox txtCodigoDistri;
         private System.Windows.Forms.TextBox txtCodigoAutor;
         private System.Windows.Forms.TextBox txtNombreLibro;
--- frmConsultaLibros.cs
+++ frmConsultaLibros.cs
@@ -13,7 +13,10 @@ namespace prySerafiniGiorgi_Biblioteca_nuevo
 {
     public partial class frmConsultaLibros : Form
     {
-        string[] baseLibro = new string[30];
+       public string[,] baseLibro = new string[20,5];
+       int indiceRecorrido = 0;
+
+
         public frmConsultaLibros()
         {
             InitializeComponent();
@@ -21,16 +24,35 @@ namespace prySerafiniGiorgi_Biblioteca_nuevo
 
         private void frmConsultaLibros_Load(object sender, EventArgs e)
         {
-            StreamReader lectorLibros = new StreamReader("./LIBRO.txt");
-            int indice = 0;
+            char separador = Convert.ToChar(",");
+            int i = 0;
 
-            while (!lectorLibros.EndOfStream)
+            StreamReader lectorLibro = new StreamReader("./LIBRO.txt");
+
+            while (!lectorLibro.EndOfStream)
             {
-                
-                baseLibro[indice] = lectorLibros.ReadLine();
-                indice++;
+                string[] VecLibro = lectorLibro.ReadLine().Split(separador);
+
+                if (i < 20)
+                {
+                    baseLibro[i, 0] = VecLibro[0];
+                    baseLibro[i, 1] = VecLibro[1];
+                    baseLibro[i, 2] = VecLibro[2];
+                    baseLibro[i, 3] = VecLibro[3];
+                    baseLibro[i, 4] = VecLibro[4];
+                    i++;
+                    
+                }
             }
-            lectorLibros.Close();
+            lectorLibro.Close();
+            txtCodigo.Text = baseLibro[0, 0];
+            txtNombreLibro.Text = baseLibro[0, 1];
+            txtCodigoEdito.Text = baseLibro[0, 2];
+            txtCodigoAutor.Text = baseLibro[0, 3];
+            txtCodigoDistri.Text = baseLibro[0, 4];
+            
+           
+            
         }
 
         private void cmdListarLibros_Click(object sender, EventArgs e)
@@ -42,20 +64,23 @@ namespace prySerafiniGiorgi_Biblioteca_nuevo
         
         private void button2_Click(object sender, EventArgs e)
         {
-            int indiceRecorrido = 0;
-            
-            
-            if (indiceRecorrido < 30 && baseLibro != null)
+            indiceRecorrido++;
+
+            txtCodigo.Text = baseLibro[indiceRecorrido, 0];
+            txtNombreLibro.Text = baseLibro[indiceRecorrido, 1];
+            txtCodigoEdito.Text = baseLibro[indiceRecorrido, 2];
+            txtCodigoAutor.Text = baseLibro[indiceRecorrido, 3];
+            txtCodigoDistri.Text = baseLibro[indiceRecorrido, 4];
+            cmdRetroceso.Enabled = true;
+
+            if (indiceRecorrido == baseLibro.GetLength(0) - 1)
             {
-                txtCodigo.Text = baseLibro[indiceRecorrido];
-                txtNombreLibro.Text = baseLibro[indiceRecorrido];
-                indiceRecorrido++;
-
-
-
+                cmdAvanzar.Enabled = false;
             }
-           
-            
+
+
+
+
 
 
 
@@ -63,7 +88,25 @@ namespace prySerafiniGiorgi_Biblioteca_nuevo
 
         private void button1_Click(object sender, EventArgs e)
         {
+            indiceRecorrido--;
+            if (indiceRecorrido >= 0)
+            {
+                txtCodigo.Text = baseLibro[indiceRecorrido, 0];
+                txtNombreLibro.Text = baseLibro[indiceRecorrido, 1];
+                txtCodigoEdito.Text = baseLibro[indiceRecorrido, 2];
+                txtCodigoAutor.Text = baseLibro[indiceRecorrido, 3];
+                txtCodigoDistri.Text = baseLibro[indiceRecorrido, 4];
 
+                if (indiceRecorrido == 0)
+                {
+                    cmdRetroceso.Enabled = false;
+                }
+
+            }
+            else
+            {
+                cmdRetroceso.Enabled = false;
+            }
         }
     }
 }
